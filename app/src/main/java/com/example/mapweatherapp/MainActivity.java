d1efb5ca3a191fa6dfeded9da21bba15952ef62d
Yera66 <yerassyl.omirserik@students.salle.url.edu>
2023-03-15 19:22:00 +0100

    bottom navigation bar with maps and weather options

--- app/src/main/java/com/example/mapweatherapp/MainActivity.java
+++ app/src/main/java/com/example/mapweatherapp/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.mapweatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    BottomNavigationView bottomNavigationView;
+
+    MapsFragment mapsFragment = new MapsFragment();
+    weatherFragment weatherFragment = new weatherFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,mapsFragment).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,weatherFragment).commit();
+
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.map:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,mapsFragment).commit();
+                        return true;
+                    case R.id.weather:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,weatherFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
